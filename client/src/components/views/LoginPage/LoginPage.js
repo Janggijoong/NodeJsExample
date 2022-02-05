6cb51475387f61646c98149fdbe7d2b1a8005441
giJoongJang <rlwnd0000@gmail.com>
2022-02-05 18:11:01 +0900

    리덕스를 이용한 로그인페이지 구현

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,9 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {useDispatch} from 'react-redux';
+import {loginUser} from '../../../_actions/user_action';
+import { useNavigate } from 'react-router-dom';
 
 function LoginPage() {
-  return <div>
-            LoginPage
-         </div>;
+    let navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const [Email, setEmail] = useState("")
+    const [Password, setPassword] = useState("")
+
+    const onEmailHandler = (event) => {
+        setEmail(event.currentTarget.value)
+    }
+
+    const onPasswordHandler = (event) => {
+        setPassword(event.currentTarget.value)
+    }
+
+    const onSummitHandler = (event) => {
+        event.preventDefault();
+
+        // console.log('Email', Email)
+        // console.log('password', Password)
+
+        let body = {
+            email: Email,
+            password: Password
+        }
+
+        dispatch(loginUser(body))
+            .then(response => {
+                if(response.payload.loginSuccess) {
+                    // props.history.push('/')
+                    navigate('/')
+                } else {
+                    alert('error')
+                }
+            })
+
+       
+    }
+
+    return (
+        <div style={{
+            display:'flex', justifyContent: 'center', alignItems: 'center',
+            width: '100%', height: '100vh'}}>
+            <form style={{display: 'flex', flexDirection: 'column'}} onSubmit={onSummitHandler}>
+                <label>Email</label>
+                <input type="email" value={Email} onChange={onEmailHandler}/>
+                <label>Password</label>
+                <input type="password" value={Password} onChange={onPasswordHandler}/>
+                <br />
+                <button>
+                    Login
+                </button>
+            </form>
+        </div>
+    )
 }
 
 export default LoginPage;
